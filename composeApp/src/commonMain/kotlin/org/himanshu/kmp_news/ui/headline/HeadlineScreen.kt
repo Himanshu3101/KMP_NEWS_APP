8ba678da736bc25c33b0e769a02c48c34e2d1b6a
Himanshu3101 <himanshusrivastava3101@gmail.com>
2025-03-18 18:29:50 +0530

    Navigation Done Lazy grid

--- composeApp/src/commonMain/kotlin/org/himanshu/kmp_news/ui/headline/HeadlineScreen.kt
+++ composeApp/src/commonMain/kotlin/org/himanshu/kmp_news/ui/headline/HeadlineScreen.kt
@@ -1,13 +1,8 @@
 package org.himanshu.kmp_news.ui.headline
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+
 import org.himanshu.kmp_news.ui.common.ArticleListScreen
 
 @Composable
